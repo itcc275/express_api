a061f84c68a7ac21faa34db6d5c047ec3fb775ea
Abhishek Kumar <itcc275@gmail.com>
2025-06-13 19:57:09 +0530

    websocket example

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express'
-import path from 'path'
+import path, { join } from 'path'
 import { fileURLToPath } from 'url'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -8,34 +10,25 @@ const __dirname = path.dirname(__filename)
 const PORT = 4000
 
 const app = express()
-
-const result = {
-    value: 'Hello, World!'
-}
+const server = createServer(app)
+const io = new Server(server)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-const queue = []
-
-app.get('/getValue', (req, res) => {
-    if (req.query.lastValue !== result.value) {
-        res.json(result)
-    } else {
-        queue.push(res)
-    }
-})
-
-app.get('/updateValue', (req, res) => {
-    result.value = req.query.value
-    res.json(result)
-    queue.forEach((response) => {
-        response.json(result)
+io.on('connection', (socket) => { // connection event
+    console.log("connection established")
+    socket.on('chat message', (msg) => { // listen for 'chat message' events here 'chat message' is the custom event
+        console.log(`Message received: ${msg}`) // log the message to the console
+        io.emit('chat message', msg) // broadcast the message to all connected clients
     })
-    queue.length = 0 // Clear the queue after sending responses
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    })
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
