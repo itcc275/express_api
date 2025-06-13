9c0ea5a54af76351737ceb31921ea25b2cbc9392
Abhishek Kumar <itcc275@gmail.com>
2025-06-13 20:19:27 +0530

    implemented server side events

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express'
-import path, { join } from 'path'
+import path from 'path'
 import { fileURLToPath } from 'url'
-import { createServer } from 'http'
-import { Server } from 'socket.io'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -10,25 +8,32 @@ const __dirname = path.dirname(__filename)
 const PORT = 4000
 
 const app = express()
-const server = createServer(app)
-const io = new Server(server)
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-io.on('connection', (socket) => { // connection event
-    console.log("connection established")
-    socket.on('chat message', (msg) => { // listen for 'chat message' events here 'chat message' is the custom event
-        console.log(`Message received: ${msg}`) // log the message to the console
-        io.emit('chat message', msg) // broadcast the message to all connected clients
-    })
+app.get('/sse', (req, res) => {
+    res.setHeader('Content-Type', 'text/event-stream')
+    res.setHeader('Cache-Control', 'no-cache')
+    res.setHeader('Connection', 'keep-alive')
 
-    socket.on('disconnect', () => {
-        console.log('user disconnected')
+    let count = 0
+    const intervalId = setInterval(() => {
+        count++
+        res.write(`data: ${count}\n\n`)
+        // if (count >= 10) {
+        //     clearInterval(intervalId)
+        //     res.end()
+        // }
+    }, 5000)
+
+    req.on('close', () => {
+        clearInterval(intervalId)
     })
 })
 
-server.listen(PORT, () => {
+
+app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
 })
