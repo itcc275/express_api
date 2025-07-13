978d52a51a1a297b7c038a8133a4bcaf5e9880b8
Abhishek Kumar <itcc275@gmail.com>
2025-07-13 10:17:06 +0530

    localstorage example

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 // const express = require('express');  recomended for node but we can use import as well
 import express from 'express'; // to use import we have to add "type": "module" in package.json
+import path from 'path'; // if using import, you can also use this
+import { fileURLToPath } from 'url'; // if using import, you can also use this
 
+// __dirname is not available in ES modules, so we need to create it
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 const PORT = 5111
 
 app.all('/', (req, res) => {
-    console.log(req);
-    console.log(res);
-    res.send('Hello World!');
+    res.sendFile(__dirname + '/index.html');
 })
 
 app.listen(PORT, () => {
